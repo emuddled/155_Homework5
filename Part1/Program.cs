ccc19219adc6c96a6cd66cb2ae834c6c5b48123c
ersin <ersinclair@gmail.com>
2020-10-05 11:15:04 -0700

    Add project files.

---
+++ Part1/Program.cs
@@ -0,0 +1,55 @@
+﻿/// Homework No. 5		Exercise Part 1
+/// File Name: ReverseArray
+/// @author: Evan Sinclair
+/// Date:  Oct 5, 2020
+///
+/// Problem Statement: Copy an Array
+/// 
+/// Overall Plan:
+/// 1)Declare and initialize an array
+/// 2)Declare a second array that will copy the first
+/// 3)Create a for loop to copy each item in the first array to the second array
+/// 4)Use foreach loop to print out all the elements of both arrays to check for correct copy
+///
+///
+
+
+
+
+
+
+
+using System;
+using System.Data;
+
+namespace Part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //first array
+            int[] firstArray = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+            
+            //second array
+            int[] copyArray = new int[10];
+            
+            //for loop to copy
+            for (int j = firstArray.Length; j > 0; j--)
+            {
+                copyArray[j - 1] = firstArray[j - 1];
+            }
+            
+            //loops to output to console
+            foreach (int printMe in firstArray)
+            {
+                Console.WriteLine(printMe.ToString());
+            }
+            foreach (int printMe in copyArray)
+            {
+                Console.WriteLine(printMe.ToString());
+            }
+        }
+    }
+}
---
+++ Part2/Program.cs
@@ -0,0 +1,73 @@
+﻿/// Homework No. 5		Exercise Part 2
+/// File Name: ReverseArray
+/// @author: Evan Sinclair
+/// Date:  Oct 5, 2020
+///
+/// Problem Statement:Reverse an array. Create three methods: one to create the array,
+/// one to reverse the array, and one to print the array at the end.
+/// 
+/// Overall Plan:
+/// 1)Create a method to produce an array of a given length filled with random integers
+/// 2)Create a method to reverse the array
+/// 3)Create a method to print the contents of the array
+/// 4)Call all 3 methods in the Main method to execute
+///
+///
+
+
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = GenerateNumbers(7);
+            PrintNumbers(arr);
+            Reverse(arr);
+            PrintNumbers(arr);
+        }
+        
+        
+        //generate int[] filled with random numbers
+        static int[] GenerateNumbers(int size)
+        {
+            Random rand = new Random();
+            int[] a = new int[size];
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] = rand.Next(11);
+            }
+            return a;
+
+        }
+
+        //reverse and array
+        static int[] Reverse(int[] arr)
+        {
+            int tempInt;
+
+            for (int i = 0; i < arr.Length / 2; i++)
+            {
+                tempInt = arr[i];
+                arr[i] = arr[arr.Length - 1 - i];
+                arr[arr.Length - 1 - i] = tempInt;
+            }
+
+            return arr;
+        }
+
+        //print an array of ints
+        static void PrintNumbers(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+
+        }
+    }
+}
